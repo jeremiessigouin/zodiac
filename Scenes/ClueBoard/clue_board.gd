90cdf138d7aa1321cedb1d45462ac14f563b2784
jeremiessigouin <jeremiessigouin@gmail.com>
2025-07-06 18:52:49 -0400

    suspect-cards-creation

--- Scenes/ClueBoard/clue_board.gd
+++ Scenes/ClueBoard/clue_board.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 @onready var polaroids_list = $HUD/Control/PolaroidsList
+@onready var suspects_list = $HUD/Control/SuspectsPanel
 
-var victim_card_scene = preload("res://Scenes/ClueBoard/VictimCards/victim_card.tscn")
-
+#var victim_card_scene = preload("res://Scenes/ClueBoard/VictimCards/victim_card.tscn")
 
 func _ready():
 	polaroids_list.hide()
+	suspects_list.hide()
 
 func _on_polaroids_button_pressed():
 	if not polaroids_list.visible:
@@ -19,4 +20,23 @@ func _on_polaroids_button_pressed():
 func _on_stephany_romy_button_pressed():
 	var card = preload("res://Scenes/ClueBoard/VictimCards/stephany_romy_card.tscn").instantiate()
 	$CardsOnBoard.add_child(card)
-	$HUD/Control/PolaroidsList/GridContainer/StephanyRomy_Button.queue_free()
+	$HUD/Control/PolaroidsList/VictimsContainer/StephanyRomy_Button.queue_free()
+
+
+func _on_suspects_button_pressed():
+	if not suspects_list.visible:
+		suspects_list.show()
+	elif suspects_list.visible:
+		suspects_list.hide()
+
+
+func _on_suspect_1_button_pressed():
+	var card = preload("res://Scenes/ClueBoard/SuspectsCards/suspect_card_template.tscn").instantiate()
+	$CardsOnBoard.add_child(card)
+	$HUD/Control/SuspectsPanel/SuspectsContainer/Suspect1Button.queue_free()
+	
+
+func _on_richard_lovings_button_2_pressed():
+	var card = preload("res://Scenes/ClueBoard/SuspectsCards/richard_lovings_card.tscn").instantiate()
+	$CardsOnBoard.add_child(card)
+	$HUD/Control/SuspectsPanel/SuspectsContainer/RichardLovingsButton2.queue_free()
